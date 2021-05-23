f49f715c33f4ad3dd6f353196ba320d409916fdd
Dong Feng <middle.fengdong@gmail.com>
2021-05-23 16:14:26 -0700

    Drawable size for multi-targets.

--- NuoWindowsFoundation/NuoDirect/NuoRenderPassTarget.cpp
+++ NuoWindowsFoundation/NuoDirect/NuoRenderPassTarget.cpp
@@ -174,4 +174,20 @@ PNuoTexture NuoRenderPassTarget::ResultTexture(unsigned int index) const
 }
 
 
+void NuoRenderPassTarget::SetDrawableSize(const NuoSize& size)
+{
+	if (_width == size.X() && _height == size.Y())
+	{
+		return;
+	}
+
+	_width = (unsigned int)size.X();
+	_height = (unsigned int)size.Y();
+
+	_views.clear();
+	_attachments.clear();
+
+	Init();
+}
+
 
