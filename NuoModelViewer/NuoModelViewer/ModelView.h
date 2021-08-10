9436d01665d597fad061da5f311d70e7e32d2f5d
Dong Feng <middle.fengdong@gmail.com>
2021-08-10 15:11:25 -0700

    Remove old code.

--- NuoModelViewer/NuoModelViewer/ModelView.h
+++ NuoModelViewer/NuoModelViewer/ModelView.h
@@ -28,17 +28,7 @@ typedef std::shared_ptr<ModelRenderer> PModelRenderer;
 
 class ModelView : public NuoDirectView
 {
-	/*PModelState _modelState;
-	
-	PNuoTextureMesh _textureMesh;
-	PNuoResourceSwapChain _light;
-	PNuoResourceSwapChain _mvp;
-
-	PNuoRenderTarget _intermediateTarget;*/
-
 	PNuoTimer _refreshTimer;
-
-	//NuoMatrixFloat44 _modelTransfer;
 
 	bool _init;
 
