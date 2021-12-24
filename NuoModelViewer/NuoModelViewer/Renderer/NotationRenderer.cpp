5b9b24c19d3597efdc5b8481aea75a62994dd7a4
Dong Feng <middle.fengdong@gmail.com>
2021-12-23 16:23:33 -0800

    Notation renderer class.

---
+++ NuoModelViewer/NuoModelViewer/Renderer/NotationRenderer.cpp
@@ -0,0 +1,11 @@
+﻿
+//
+//  NotationRenderer.cpp
+//  ModelViewer
+//
+//  Created by Dong Feng on 12/23/21.
+//  Copyright © 2021 Dong Feng. All rights reserved.
+//
+
+
+#include "NotationLight.h"
---
+++ NuoModelViewer/NuoModelViewer/Renderer/NotationRenderer.h
@@ -0,0 +1,34 @@
+
+
+
+//
+//  NotationRenderer.h
+//  ModelViewer
+//
+//  Created by Dong Feng on 12/23/21.
+//  Copyright © 2021 Dong Feng. All rights reserved.
+//
+
+
+#ifndef __NOTATION_RENDERER_H__
+#define __NOTATION_RENDERER_H__
+
+#pragma once
+
+#include "NuoRender/NuoRenderPipelinePass.h"
+
+#include "NuoUtilites/NuoMathVector.h"
+
+#include <dxgi1_6.h>
+
+
+
+class Notation : public NuoRenderPipelinePass
+{
+
+public:
+
+};
+
+
+#endif
