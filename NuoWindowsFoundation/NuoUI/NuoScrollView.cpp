5d7d6a7967f35e78df9ef42e5fec1338640945b5
Dong Feng <middle.fengdong@gmail.com>
2022-04-26 17:31:30 -0700

    Scroll view class stub.

---
+++ NuoWindowsFoundation/NuoUI/NuoScrollView.cpp
@@ -0,0 +1,64 @@
+
+
+//
+//  NuoScrollView.cpp
+//  ModelViewer
+//
+//  Created by Dong Feng on 4/26/22.
+//  Copyright © 2022 Dong Feng. All rights reserved.
+//
+
+
+
+#include "NuoScrollView.h"
+
+#include "NuoStrings.h"
+#include "NuoAppInstance.h"
+#include "NuoDialog.h"
+
+
+NuoScrollView::NuoScrollView(const PNuoWindow& parent, const std::string& title)
+    : NuoControl(title, parent)
+{
+}
+
+
+NuoScrollView::NuoScrollView(const PNuoDialog& parent, int controlID)
+    : NuoControl("", std::dynamic_pointer_cast<NuoWindow>(parent))
+{
+    _hWnd = GetDlgItem(parent->Handle(), controlID);
+}
+
+
+void NuoScrollView::Init(int controlID)
+{
+    std::wstring wtitle = StringToUTF16(_title);
+    PNuoWindow parent = _parent.lock();
+
+    _hWnd = CreateWindow(L"BUTTON",     // Predefined class; Unicode assumed 
+        wtitle.c_str(),        // Button text 
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_FLAT | BS_PUSHBUTTON,  // Styles 
+        10, 10, 300, 40,       // Button height
+        parent->Handle(),      // Parent window
+        (HMENU)controlID,
+        NuoAppInstance::GetInstance()->Instance(),
+        NULL);
+
+    _id = controlID;
+
+    SetWindowLongPtr(_hWnd, kWindowPtr, (LONG_PTR)this);
+    parent->Add(shared_from_this());
+
+    Update();
+    Show();
+}
+
+
+NuoScrollView::~NuoScrollView()
+{
+    if (_hWnd)
+    {
+        DestroyWindow(_hWnd);
+        _hWnd = 0;
+    }
+}
