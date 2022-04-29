eb504de51e0b28422fb2d1d7a24553651fd3242f
Dong Feng <middle.fengdong@gmail.com>
2022-04-29 11:04:26 -0700

    Model Operation Panel class stub.

---
+++ NuoModelViewer/NuoModelViewer/UserInterface/ModelOperationPanel.cpp
@@ -0,0 +1,20 @@
+//
+//  ModelOperationPanel.cpp
+//  ModelViewer
+//
+//  Created by middleware on 4/29/22.
+//  Copyright © 2022 Dong Feng. All rights reserved.
+//
+
+#include "ModelOperationPanel.h"
+
+
+ModelOperationPanel::ModelOperationPanel(const PNuoWindow& parent)
+	: NuoView(parent)
+{
+}
+
+
+ModelOperationPanel::~ModelOperationPanel()
+{
+}
---
+++ NuoModelViewer/NuoModelViewer/UserInterface/ModelOperationPanel.h
@@ -0,0 +1,22 @@
+//
+//  ModelOperationPanel.h
+//  ModelViewer
+//
+//  Created by middleware on 4/29/22.
+//  Copyright © 2022 Dong Feng. All rights reserved.
+//
+
+
+#include "NuoUI/NuoView.h"
+#include "NuoUI/NuoScrollView.h"
+
+
+class ModelOperationPanel : public NuoView
+{
+
+public:
+
+	ModelOperationPanel(const PNuoWindow& parent);
+	virtual ~ModelOperationPanel();
+
+};
