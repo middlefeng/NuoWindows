e45fd9a13e5341c080fa1133a5f870174a9c9247
Dong Feng <middle.fengdong@gmail.com>
2022-05-04 22:36:09 -0700

    Model scene parameters class skeleton.

---
+++ NuoModelViewer/NuoModelViewer/Renderer/ModelSceneParameters.cpp
@@ -0,0 +1,16 @@
+﻿//
+//  ModelSceneParameters.m
+//  ModelViewer
+//
+//  Created by Dong on 7/26/19.
+//  Ported 5/4/22
+//  Copyright © 2022 Dong Feng. All rights reserved.
+//
+
+
+
+#include "ModelSceneParameters.h"
+
+
+
+
---
+++ NuoModelViewer/NuoModelViewer/Renderer/ModelSceneParameters.h
@@ -0,0 +1,28 @@
+//
+//  ModelSceneParameters.h
+//  ModelViewer
+//
+//  Created by Dong on 7/26/19.
+//  Ported 5/4/2022
+//  Copyright © 2022 Dong Feng. All rights reserved.
+//
+
+
+#ifndef __MODEL_SCENE_PARAMETERS_H__
+#define __MODEL_SCENE_PARAMETERS_H__
+
+#pragma once
+
+
+
+
+
+class ModelSceneParameters
+{
+
+public:
+
+};
+
+
+#endif
